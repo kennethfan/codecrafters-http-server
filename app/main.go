f3f53e9645231827c0562433add45af1c88200ef
kennethfan <kennethfan137@gmail.com>
2024-12-17 20:59:26 +0800

    test feature

--- app/main.go
+++ app/main.go
@@ -5,7 +5,11 @@ import (
 	"github.com/kennethfan/codecrafters-http-server/example"
 	"github.com/kennethfan/codecrafters-http-server/http"
 	"github.com/kennethfan/codecrafters-http-server/http/common"
+	"github.com/kennethfan/codecrafters-http-server/http/component"
+	"github.com/kennethfan/codecrafters-http-server/http/component/builtin"
 	"github.com/kennethfan/codecrafters-http-server/http/core"
+	"github.com/kennethfan/codecrafters-http-server/util"
+	"log"
 	"net"
 	"os"
 )
@@ -25,11 +29,19 @@ func main() {
 	//server := http.NewServer(http.WithAddress("0.0.0.0:4221"), http.WithRouter(http.NewRouter()))
 	router := server.Router()
 	router.RegisterSimpleHandleFunc("/", func(request *core.Request, response *core.Response) error {
-		response.StatusOK()
-		response.End()
+		util.Response200(response)
 		return nil
 	})
 	router.RegisterSimpleHandleFunc("/index.html", example.Html, common.HttpMethodGet)
 	router.RegisterSimpleHandleFunc("/status.json", example.Json, common.HttpMethodGet)
+	mimeTypes, err := component.MimeTypeFromFile("resources/mime.types")
+	if err != nil {
+		log.Println("build mimetypes error, file=resources/mime.types")
+	} else {
+		router.RegisterPatternHandler(1, "/static/.+", builtin.NewStaticHandler("/static/", "static/", mimeTypes), common.HttpMethodGet)
+	}
+
+	middlewares := server.Middlewares()
+	middlewares.RegisterPatternMiddleware(1, "/.*", &builtin.LogMiddleware{})
 	server.Start()
 }
